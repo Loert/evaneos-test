a2aeb71996d5b150db93fc61cafa4b48edb4b98c
Rollet Thomas <t.rollet51@gmail.com>
2020-01-20 21:36:21 +0100

    refact: ApplicationContext

--- src/Context/ApplicationContext.php
+++ src/Context/ApplicationContext.php
@@ -1,5 +1,10 @@
 <?php
 
+use Faker\Factory;
+
+/**
+ * Class ApplicationContext
+ */
 class ApplicationContext
 {
     use SingletonTrait;
@@ -13,19 +18,28 @@ class ApplicationContext
      */
     private $currentUser;
 
+    /**
+     * ApplicationContext constructor.
+     */
     protected function __construct()
     {
-        $faker = \Faker\Factory::create();
+        $faker = Factory::create();
         $this->currentSite = new Site($faker->randomNumber(), $faker->url);
         $this->currentUser = new User($faker->randomNumber(), $faker->firstName, $faker->lastName, $faker->email);
     }
 
-    public function getCurrentSite()
+    /**
+     * @return Site
+     */
+    public function getCurrentSite(): Site
     {
         return $this->currentSite;
     }
 
-    public function getCurrentUser()
+    /**
+     * @return User
+     */
+    public function getCurrentUser(): User
     {
         return $this->currentUser;
     }
